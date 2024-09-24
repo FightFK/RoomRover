996c3983ac38d5b3f4166b3f0b395699fc8ea0f8
NYUBNOOB <tinritto@gmail.com>
2024-09-24 14:11:58 +0700

    AddGetstartpage

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Home from './app/Pages/Home';  // Path to your Home component
+import Getstart from './app/Pages/Getstart';
 import BottomNavbar from './app/Pages/Nav/nav'; // Path to your BottomNavbar component
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Home /> 
-     
+      <Getstart /> 
     </View>
   );
 }
---
+++ app/Pages/Getstart.js
@@ -0,0 +1,55 @@
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity  } from 'react-native';
+
+export default function Getstart(props) {
+    
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={require('../../assets/Room.png')} style={styles.image}>
+                <Text style={styles.textapp}>RoomRover</Text>
+                <Text style={styles.textabout}>Roomservice & Management</Text>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>GET START</Text>
+                </TouchableOpacity>
+            </ImageBackground>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    image: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
+    textapp: {
+        color: 'white',
+        fontSize: 48,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontStyle: 'italic',
+    },
+    textabout: {
+        color: 'white',
+        fontSize: 14,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontStyle: 'normal',
+    },
+    button: {
+        backgroundColor: '#29B6F6',
+        paddingVertical: 15,
+        borderRadius: 5,
+        margin: 48,
+    },
+    buttonText: {
+        color: '#fff', // Change text color here
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+
+});
